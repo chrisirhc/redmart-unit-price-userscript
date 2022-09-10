f601f8f3f10d8122d24e203f52a099b69aa4e679
Chris Chua <chris.sirhc@gmail.com>
2022-09-10 14:38:53 +0800

    add bundle task definition

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -3,7 +3,8 @@ import { ensureDir } from "https://deno.land/std@0.151.0/fs/ensure_dir.ts";
 
 const ts = await Deno.readTextFile("./main.ts");
 const result = await esbuild.transform(ts, { loader: "ts" });
-console.log("result:", result);
 esbuild.stop();
 ensureDir("./build");
 await Deno.writeTextFile("./build/main.js", result.code);
+console.log("ran successfully");
+console.log("result:", result);
