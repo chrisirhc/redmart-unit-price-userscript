435cfcf9e5d74bfed37000ccb3d086217228032a
Chris Chua <chris.sirhc@gmail.com>
2022-09-10 22:21:40 +0800

    fix bundle script to respect entrypoint
    Stop polluting window.

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,10 +1,8 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.15.7/mod.js";
 import { ensureDir } from "https://deno.land/std@0.151.0/fs/ensure_dir.ts";
 
-const ts = await Deno.readTextFile("./main.ts");
-const result = await esbuild.transform(ts, { loader: "ts" });
-esbuild.stop();
 ensureDir("./build");
-await Deno.writeTextFile("./build/main.js", result.code);
+const result = await esbuild.build({entryPoints: ['./main.ts'], outdir:'./build', bundle: true});
+esbuild.stop();
 console.log("ran successfully");
 console.log("result:", result);
